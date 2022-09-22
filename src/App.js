1f82518f5a3bdadbb0923edee8a6013a84557834
wedgles97 <wedegles@gmail.com>
2022-09-21 22:43:33 -0300

    estilo da pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+          type="text"
+          placeholder="Digite seu CEP..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 85698456</h2>
+
+        <span>Rua Teste algum</span>
+        <span>Complemento: algum complemento</span>
+        <span>Bairro</span>
+        <span>Cidade - Estado</span>
+
+      </main>
+
     </div>
   );
 }

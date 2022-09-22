d0dab95fb3353a0988144d56a193748addbacdb8
wedgles97 <wedegles@gmail.com>
2022-09-21 23:22:10 -0300

    api axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
 
+import api from './services/api'
+
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if(input === '') {
+      alert("Preencha algum CEP!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("");
+    }catch{
+      alert("Ops erro ao buscar");
+      setInput("")
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -11,22 +34,26 @@ function App() {
         <input 
           type="text"
           placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 85698456</h2>
-
-        <span>Rua Teste algum</span>
-        <span>Complemento: algum complemento</span>
-        <span>Bairro</span>
-        <span>Cidade - Estado</span>
-
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+  
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+  
+        </main>
+      )}
 
     </div>
   );
